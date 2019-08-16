3ad045f24a87ad88e1bdba8c91e80ca310fb52b9
Chester Abrahams <43809102+ChesterOfTheSemester@users.noreply.github.com>
2019-08-16 23:23:42 +0100

    Create XOR_Swap.asm

---
+++ XOR_Swap.asm
@@ -0,0 +1,15 @@
+; XOR Swap without 3rd data unit
+swap:
+        PUSH    RBP
+        MOV     RBP, RSP
+        MOV     DWORD PTR [RBP-4], EDI ; arg int x
+        MOV     DWORD PTR [RBP-8], ESI ; arg int y
+        MOV     EAX, DWORD PTR [RBP-8] ; x XOR y
+        XOR     DWORD PTR [RBP-4], EAX
+        MOV     EAX, DWORD PTR [RBP-4] ; y XOR x
+        XOR     DWORD PTR [RBP-8], EAX
+        MOV     EAX, DWORD PTR [RBP-8] ; x XOR y
+        XOR     DWORD PTR [RBP-4], EAX
+        NOP
+        POP     RBP
+        RET
